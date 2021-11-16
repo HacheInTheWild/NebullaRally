1ceb719052b20a498b6a3c02775678a6aeb3bc09
LuKmf <lucas.fur99@gmail.com>
2021-11-16 22:24:35 +0100

    update hud
    update hud
    navegacion entre escenas

--- Nebulla Rally - unity/Assets/Scripts/Botones/Buttons.cs
+++ Nebulla Rally - unity/Assets/Scripts/Botones/Buttons.cs
@@ -5,8 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-  
 
+    //Title Scene -> Main Menu
+    public void Jugar()
+    {
+        SceneManager.LoadScene(1);
+    }
 
+    //Main Menu -> Nivel 1
+    public void Carrera()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SalirJuego()
+    {
+        Application.Quit();
+        Debug.Log("Cerrando juego...");
+    }
+    
+
+    //Settings
+    //Seleccionar nivel
+    //Cafe
+    //Home
+    //Garaje
+    //Volver a atras
 
 }
