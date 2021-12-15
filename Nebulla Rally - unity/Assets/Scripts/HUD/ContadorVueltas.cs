444d0a032ef73c694f495838cc7eb65b8c897d46
JAGARHE2 <jvrgrchrmsll@gmail.com>
2021-12-16 00:54:22 +0100

    arreglos

--- Nebulla Rally - unity/Assets/Scripts/HUD/ContadorVueltas.cs
+++ Nebulla Rally - unity/Assets/Scripts/HUD/ContadorVueltas.cs
@@ -2,26 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ContadorVueltas : MonoBehaviour
 {
     public int vueltas = 0;
     public Text contadorVueltas;
+    public Text GameOver;
+    public Text Reset;
     public GameObject ObjPuntos;
 
     void Start()
     {
-        
+        GameOver.enabled = false;
+        Reset.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         contadorVueltas.text = vueltas.ToString() + "/3";
-        if ( vueltas == 3)
+        if ( vueltas > 3)
         {
+            contadorVueltas.text = "3/3";
             GameObject nave = GameObject.Find("Nave");
+            GameOver.enabled = true;
+            Reset.enabled = true;
             nave.GetComponent<movimientoNave>().enabled = false;
+            if (Input.GetKey(KeyCode.R))
+            {
+                SceneManager.LoadScene("Racing Map 1");
+            }
             //nave.GetComponent<movimientoNave>().speed = 0;
         }
     }
